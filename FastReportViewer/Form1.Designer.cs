558115aa3c550aacdadc8ee5741048f937468e6c
Samet Çetin <samet.cetin@murat.com.tr>
2018-09-27 14:15:41 +0300

    added files

---
+++ FastReportViewer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 12);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(324, 20);
+            this.txtFileName.TabIndex = 0;
+            this.txtFileName.TextChanged += new System.EventHandler(this.txtFileName_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(82, 65);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(177, 65);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 111);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileName);
+            this.MaximumSize = new System.Drawing.Size(365, 150);
+            this.MinimumSize = new System.Drawing.Size(365, 150);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnOpen;
+        private FastReport.Report barcodeView;
+    }
+}
+
---
+++ FastReportViewer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+             
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Select file";
+            //fileDialog.InitialDirectory = @"C:\";
+            //fileDialog.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fileDialog.Filter = "frx files(*.frx)|*.frx";
+
+            //Example: "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            fileDialog.FilterIndex = 2;
+            fileDialog.RestoreDirectory = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(fileDialog.FileName))
+                    ;
+                else
+                    txtFileName.Text = fileDialog.FileName;
+            }
+            
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                barcodeView = new FastReport.Report();
+                barcodeView.PrintSettings.ShowDialog = false;
+                barcodeView.PrintSettings.Printer = "Pr0";
+                barcodeView.Load(txtFileName.Text);
+                barcodeView.Design();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void txtFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Haffner Barcode Designer, V18.01";
+        }
+    }
+}
